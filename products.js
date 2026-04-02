bdcd4c12a24e61aefa60a8cb43f95502beda1fe6
mathuradressbhandaar <mathura.dress.bhandaar@gmail.com>
2026-04-02 23:41:04 +0530

    Add products.js

---
+++ products.js
@@ -0,0 +1,92 @@
+const PRODUCTS = [
+  {
+    id: 1,
+    name: "Krishna Dress 1",
+    size: "M",
+    price: 226,
+    stock: 10,
+    image: "https://res.cloudinary.com/dhor52cru/image/upload/f_auto,q_auto/sample_poshak1_f8mphs",
+    stock_status: "in_stock"
+  },
+  {
+    id: 2,
+    name: "Krishna Dress 2",
+    size: "M",
+    price: 276,
+    stock: 9,
+    image: "https://res.cloudinary.com/dhor52cru/image/upload/f_auto,q_auto/v1774530835/poshak5_l4izla.jpg",
+    stock_status: "in_stock"
+  },
+  {
+    id: 3,
+    name: "Krishna Dress 3",
+    size: "M",
+    price: 314,
+    stock: 3,
+    image: "https://res.cloudinary.com/dhor52cru/image/upload/v1774530836/poshak7_ov5qd3.jpg",
+    stock_status: "in_stock"
+  },
+  {
+    id: 4,
+    name: "Krishna Dress 4",
+    size: "M",
+    price: 245,
+    stock: 4,
+    image: "https://res.cloudinary.com/dhor52cru/image/upload/v1774530836/poshak6_dvrtqc.jpg",
+    stock_status: "in_stock"
+  },
+  {
+    id: 5,
+    name: "Krishna Dress 5",
+    size: "M",
+    price: 238,
+    stock: 10,
+    image: "https://res.cloudinary.com/dhor52cru/image/upload/v1774530834/poshak4_wwn0zh.jpg",
+    stock_status: "in_stock"
+  },
+  {
+    id: 6,
+    name: "Krishna Dress 6",
+    size: "M",
+    price: 139,
+    stock: 15,
+    image: "https://res.cloudinary.com/dhor52cru/image/upload/v1774530831/poshak3_iuqy3c.jpg",
+    stock_status: "in_stock"
+  },
+  {
+    id: 7,
+    name: "Krishna Dress 7",
+    size: "M",
+    price: 432,
+    stock: 17,
+    image: "https://res.cloudinary.com/dhor52cru/image/upload/v1774530830/poshak2_va9j0q.jpg",
+    stock_status: "in_stock"
+  },
+  {
+    id: 8,
+    name: "Krishna Dress 8",
+    size: "M",
+    price: 241,
+    stock: 54,
+    image: "https://res.cloudinary.com/dhor52cru/image/upload/v1774530826/dress3_ridtlf.webp",
+    stock_status: "in_stock"
+  },
+  {
+    id: 9,
+    name: "Krishna Dress 9",
+    size: "M",
+    price: 139,
+    stock: 1,
+    image: "https://res.cloudinary.com/dhor52cru/image/upload/v1774530821/dress1_qwdggs.webp",
+    stock_status: "in_stock"
+  },
+  {
+    id: 10,
+    name: "Krishna Dress 10",
+    size: "M",
+    price: 327,
+    stock: 2,
+    image: "https://res.cloudinary.com/dhor52cru/image/upload/v1774530815/9_hxokn0.jpg",
+    stock_status: "in_stock"
+  }
+];
